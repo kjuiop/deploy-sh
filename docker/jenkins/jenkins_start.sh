d33c9302e2fcc608bfa5c08e57ee73b07c269f56
kjuiop <copying_y@naver.com>
2022-10-05 17:41:17 +0900

    chore : jenkins_start update

--- docker/jenkins/jenkins_start.sh
+++ docker/jenkins/jenkins_start.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
 
+# jenkins start
 sudo docker run -d -p 8080:8080 -v /jenkins:/var/jenkins_home --name jenkins -u root jenkins/jenkins:lts
+
+
+# jenkins check init password
+# docker exec -it jenkins bash -c "cat/var/jenkins_home/secrets/initialAdminPassword"
