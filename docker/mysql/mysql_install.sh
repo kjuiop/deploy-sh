f161e5d1542fab16b4cb5f6b390e05b663e0258d
kjuiop <copying_y@naver.com>
2022-10-19 19:35:07 +0900

    chore : mysql_install, start sh create

---
+++ docker/mysql/mysql_install.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# docker version 확인
+docker -v
+
+# mysql image pull
+docker pull mysql:8.0.22
+
+# image 확인
+docker images
+
---
+++ docker/mysql/mysql_start.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+# docker start
+docker run --name mysql-container -e MYSQL_ROOT_PASSWORD=1234 -d -p 3306:3306 mysql:latest
